c6f3df44deb54325633e073ee2f26b9dea5cffce
Josefie Zynga <aditi@posteo.de>
2017-09-22 19:50:58 +0200

    refactor game class

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,55 +9,58 @@ const CATEGORIES = {
   shapes: ['rectangle', 'oval', 'wave']
 };
 
-const cards = createCards();
-
-function getRandomIndex() {
-  return Math.floor(Math.random() * cards.length);
-}
-
-function getRandomCards(number) {
-  let nextCards = [];
-
-  for (var i = 0; i < number; i++) {
-    let removedCard = cards.splice(getRandomIndex(), 1)[0];
-    nextCards.push(removedCard);
-    console.log(cards.length + ' cards left!');
-  }
-  
-  return nextCards;
-}
-
-function createCards() {
-  let cards = [];
-  let count = 0;
-
-  for (var number = 0; number < 3; number++) {
-    for (var color = 0; color < 3; color++) {
-      for (var texture = 0; texture < 3; texture++) {
-        for (var shape = 0; shape < 3; shape++) {
-          let card = {};
-          card.id = count++;
-          card.number = CATEGORIES.numbers[number];
-          card.color = CATEGORIES.colors[color];
-          card.texture = CATEGORIES.textures[texture];
-          card.shape = CATEGORIES.shapes[shape];
-          cards.push(card);
-
-          // TODO: überschneidungen auslassen
-          // (3*3*3*3) - (3*4) = 69
-        }
-      }
-    }
-  }
-
-  console.log(cards);
-  return cards;
-}
-
 class Game extends Component {
   constructor(props) {
     super(props);
-    this.currentCards = getRandomCards(12);
+    this.cards = this.createCards();
+    this.currentCards = this.getRandomCards(12);
+  }
+
+  createCards() {
+    let cards = [];
+    let count = 0;
+
+    for (var number = 0; number < 3; number++) {
+      for (var color = 0; color < 3; color++) {
+        for (var texture = 0; texture < 3; texture++) {
+          for (var shape = 0; shape < 3; shape++) {
+            let card = {};
+            card.id = count++;
+            card.number = CATEGORIES.numbers[number];
+            card.color = CATEGORIES.colors[color];
+            card.texture = CATEGORIES.textures[texture];
+            card.shape = CATEGORIES.shapes[shape];
+            cards.push(card);
+
+            // TODO: überschneidungen auslassen
+            // (3*3*3*3) - (3*4) = 69
+          }
+        }
+      }
+    }
+
+    console.log(cards);
+    return cards;
+  }
+
+  getRandomIndex() {
+    return Math.floor(Math.random() * this.cards.length);
+  }
+
+  getRandomCards(number) {
+    let nextCards = [];
+
+    for (var i = 0; i < number; i++) {
+      let removedCard = this.cards.splice(this.getRandomIndex(), 1)[0];
+      nextCards.push(removedCard);
+      console.log(this.getNumberOfCards() + ' cards left!');
+    }
+    
+    return nextCards;
+  }
+
+  getNumberOfCards() {
+    return this.cards.length;
   }
 
   render() {
