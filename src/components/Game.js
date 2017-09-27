28fef6c6733a507705a9a67bab1d6f75c68a4f72
Josefie Zynga <aditi@posteo.de>
2017-09-27 23:30:37 +0200

    move card creation to a seperate class

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,41 +3,9 @@ import Board from './Board.js';
 import Timer from './Timer.js';
 import StatusBar from './StatusBar.js';
 import RestartButton from './RestartButton.js';
+import Cards from '../Cards.js';
 
-const CATEGORIES = {
-  numbers: [1, 2, 3],
-  colors: ['red', 'green', 'yellow'],
-  textures: ['solid', 'empty', 'granular'],
-  shapes: ['rectangle', 'oval', 'wave']
-};
-
-const CARDS = createCards();
-
-function createCards() {
-  let cards = [];
-  let count = 0;
-
-  for (var number = 0; number < 3; number++) {
-    for (var color = 0; color < 3; color++) {
-      for (var texture = 0; texture < 3; texture++) {
-        for (var shape = 0; shape < 3; shape++) {
-          let card = {};
-          card.id = count++;
-          card.number = CATEGORIES.numbers[number];
-          card.color = CATEGORIES.colors[color];
-          card.texture = CATEGORIES.textures[texture];
-          card.shape = CATEGORIES.shapes[shape];
-          cards.push(card);
-
-          // TODO: überschneidungen auslassen
-          // (3*3*3*3) - (3*4) = 69
-        }
-      }
-    }
-  }
-
-  return cards;
-}
+const CARDS = Cards.create();
 
 class Game extends Component {
 
